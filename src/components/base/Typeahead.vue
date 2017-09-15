b1b4c7befd1ee1979a159b2bc4574eaf931e198a
Martín Weingart <mweingart@neuquen.gov.ar>
2017-09-15 14:15:35 -0300

    Desarrollo de un componente de Autocompletado

---
+++ src/components/base/Typeahead.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <v-text-field
+      :label="label"
+      v-model="text"
+      class="pb-0"
+      @input="update"
+      @keyup.down="down"
+      @keyup.up="up"
+      @keyup.enter="enter"
+    >
+    </v-text-field>
+
+    <v-list v-if="items_filter.length && show_items">
+      <v-list-tile :class="{ 'grey lighten-2': i==i_active }"
+        v-for="(item, i) of items_filter" key="item.value" @click="setText(item)"
+        @mouseover="i_active = i
+      >
+        <v-list-tile-content>
+            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'typeahead',
+  props: ['label', 'items'],
+
+  data () {
+    return {
+      show_items: false,
+      text: '',
+      i_active: 0
+    }
+  },
+
+  created: function() {
+  },
+
+  computed: {
+    items_filter: function() {
+      if (this.items) {
+        return this.items.filter(item =>
+          item.text.toLowerCase().includes(this.text.toLowerCase())
+        );
+      }
+      else return [];
+    }
+  },
+
+  methods: {
+    setText: function(item) {
+      this.text = item.text;
+      this.$emit('input', item.value);
+      this.show_items = false;
+    },
+
+    update: function() {
+      if (this.text.length > 0) this.show_items = true;
+      else this.show_items = false;
+
+      this.i_active = 0;
+      this.items_filter[this.i_active].active = true;
+    },
+
+    down: function() {
+      if (this.i_active + 1 === this.items_filter.length) this.i_active = 0;
+      else this.i_active = this.i_active + 1;
+    },
+
+    up: function() {
+      if (!this.i_active) this.i_active = this.items_filter.length - 1;
+      else this.i_active = this.i_active - 1;
+    },
+
+    enter: function() {
+      this.setText(this.items_filter[this.i_active]);
+      this.i_active = 0;
+    },
+  },
+
+}
+</script>
+
+<style>
+</style>
