9e2b2a5ca29d2a620f0165760771068aa7817dfa
Martin Weingart <martinweingart@gmail.com>
2017-11-16 18:10:46 -0300

    Inicio de desarrollo de login

---
+++ src/components/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Autenticación</v-toolbar-title>
+    </v-toolbar>
+
+    <v-card class="">
+      <v-card-text>
+        <v-text-field
+          label="Nombre de Usuario"
+          v-model="usuario.id"
+        >
+        </v-text-field>
+        <br>
+        <v-text-field
+          label="Nombre de Usuario"
+          v-model="usuario.password"
+        >
+        </v-text-field>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import * as axios from 'axios';
+
+const User = () => ({
+  id: '',
+  password: ''
+})
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      usuario: User()
+    }
+  },
+
+  created: function() {
+  },
+
+  methods: {
+  },
+
+}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/Login';
 import { NuevaSolicitudEmpresa, NuevaSolicitudProfesional, ListaSolicitud } from '@/components/solicitudes'
 import { Matriculas, ListaMatriculados,
   MatriculaProfesional, ResumenCuenta,
