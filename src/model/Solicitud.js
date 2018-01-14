f337d907f9220725efc1fa6e8d3bbc8146fe5db5
Martin Weingart <martinweingart@gmail.com>
2018-01-13 22:02:26 -0300

    - Fix en domicilios de MatriculaDetalle. - Fix en delegacion automatica segun usuario en Nueva Solicitud.

--- src/model/Solicitud.js
+++ src/model/Solicitud.js
@@ -1,12 +1,11 @@
 import { Profesional } from '@/model/Profesional'
 import { Empresa } from '@/model/Empresa'
-import Store from '@/Store'
 import * as moment from 'moment'
 
 export class Solicitud {
   constructor(tipo) {
     this.fecha = moment().format('DD/MM/YYYY');
-    this.delegacion = Store.state.delegacion;
+    this.delegacion = '';
     if (tipo == 'profesional')
       this.entidad = new Profesional();
     else if (tipo == 'empresa')
