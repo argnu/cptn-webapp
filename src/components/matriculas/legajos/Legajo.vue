4871716179e468a6b0f4f33d07930d99778bd576
Martin Weingart <martinweingart@gmail.com>
2018-06-21 19:27:08 -0300

    Fix mensaje error al agregar legajo

--- src/components/matriculas/legajos/Legajo.vue
+++ src/components/matriculas/legajos/Legajo.vue
@@ -942,7 +942,13 @@ export default {
            })
            .catch(e => {
              this.submitted = false;
-             this.submitError(e);
+             if (e.response && e.response.status != 500) {
+              let msg = (!e.response || e.response.status == 500) ? 'Ha ocurrido un error en la conexión' : e.response.data.msg;
+              this.global_state.snackbar.msg = msg;
+              this.global_state.snackbar.color = 'error';
+              this.global_state.snackbar.show = true;
+             }
+             else console.error(e);
            });
     },
 
